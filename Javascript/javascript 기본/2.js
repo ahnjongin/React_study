329ac85fe324bb3a8e0bc62046e1f2144526c02f
ahnjongin <goodtime1998@naver.com>
2022-04-20 23:44:47 +0900

    자료형

---
+++ Javascript/javascript 기본/2.js
@@ -0,0 +1,23 @@
+let age = 25; //정수
+let tall = 175.9; //실수
+console.log(age * tall);
+
+let inf = Infinity;
+let minusInf = -Infinity;
+let nan = Nan;
+
+let name = "안종인";
+let name2 = `짱 ${name}`;
+console.log(name2); //짱 안종인
+
+let numberA = 12;
+let numberB = "2";
+console.log(numberA * numberB); //24
+
+let numberA = 12;
+let numberB = "2";
+console.log(numberA + numberB); //122
+
+let numberA = 12;
+let numberB = "2";
+console.log(numberA + parseInt(numberB));   //14
