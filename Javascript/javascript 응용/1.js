b2692d68bf424a2209a86b644727098d2ec2765a
ahnjongin <goodtime1998@naver.com>
2022-04-30 20:59:22 +0900

    Javascript응용

---
+++ Javascript/javascript 응용/1.js
@@ -0,0 +1,22 @@
+let a = ""; //FALSE
+let a = "unefined"; //FALSE
+let a = []; //TRUE
+let a = "string"  //TRUE
+//-0, NaN, 0, null - FALSE
+
+if (a){
+  console.log("TRUE");
+}
+else {
+  console.log("FALSE");
+}
+
+const getName = (person) => {
+  if(!person){  //underined 이나 null 일때의 예외처리
+    return "객체가 아닙니다";
+  }
+  return person.name;
+};
+let person = {name: "이정환"};
+const name = getName(person);
+console.log(name);
